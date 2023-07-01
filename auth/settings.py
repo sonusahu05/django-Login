3476924e409ed360dc0163af082e7740d4e9744e
Sonu Sahu <sonu.sahu@spit.ac.in>
2023-07-01 13:24:59 +0530

    user login auth done

--- auth/settings.py
+++ auth/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'login.apps.LoginConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'auth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'auth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'auth',
+        'USER': 'admin',
+        'PASSWORD': 'Sonuawsauth',
+        'HOST':'authentication.ce3v5gwcmm1a.ap-southeast-2.rds.amazonaws.com'
     }
 }
 
---
+++ login/views.py
@@ -0,0 +1,45 @@
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+
+def home(request):
+    return render(request, 'home.html', {'name':'Siddharth'})
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        username = request.POST['username']
+        confirm_password = request.POST['confirm_password']
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+            else:
+                user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                messages.info(request, 'User Created')
+                messages.info(request, 'Please Login')
+        else:
+            messages.info(request, 'Password not matching')
+    return redirect('/')
+
+
+def login(request):
+    username = request.POST['login_username']
+    password = request.POST['login_password']
+
+    user = auth.authenticate(username=username, password=password)
+    if user is not None:
+        auth.login(request, user)
+        return render(request, 'success_login.html', {'name': username})
+    else:
+        messages.info(request, 'Invalid Credentials')
+        return redirect('/#')
+
+
+
